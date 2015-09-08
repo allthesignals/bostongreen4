51c8818cccdc029b357d837fbc4d062881431d8d
wilburnforce@gmail.com <mattgardner@Matts-iMac.local>
2015-09-07 22:53:36 -0400

    Added working activity filter that's exposed through the jsonapi

--- app/models/park.rb
+++ app/models/park.rb
@@ -2,8 +2,11 @@ class Park < ActiveRecord::Base
 	self.table_name = 'public.parks'
 	self.primary_key = 'id_0'
 
-	def self.filter_by_facility(facility_id)
-		Park.where{st_intersects(geom,Facility.find(facility_id).geom)}.first
+	def self.filter_by_activity(activity_type_id)
+		facilities = Activity.find(activity_type_id).facilities.map(&:id).join(", ")
+		query = "SELECT parks.id_0 FROM parks, facilities WHERE facilities.id IN ( #{facilities} ) AND ST_Intersects(parks.geom, facilities.geom)"
+		array = Park.find_by_sql(query)
+		Park.where(id: array.map(&:id))
 	end
 
 	def self.filter_by_neighborhood(neighborhood_id)
--- app/resources/park_resource.rb
+++ app/resources/park_resource.rb
@@ -1,6 +1,17 @@
 class ParkResource < JSONAPI::Resource
 	attribute :name
 
-	filter :name
+	filters :name, :activity_type
+
+	def self.apply_filter(records, filter, value, options)
+	  	case filter
+	    	when :activity_type
+	    		puts "#{value}"
+	      		return records.filter_by_activity(value[0])
+	      	else
+	      		return super(records, filter, value)
+  		end
+	end
+	# Park.filter_by_facility(options[:context])
 
 end
